3b36449d639541a8a51a6f5309c67a45619e9984
DrikaDev <adriguti.adriana@hotmail.com>
2022-07-21 18:32:02 -0300

    Updates

---
+++ precache-manifest.a1f91997b724ac5eb4be84214ac0ae2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6eb72cc29aa025bd58b",
+    "url": "/Pratica-react-06-set-state/static/css/main.69c93921.chunk.css"
+  },
+  {
+    "revision": "d6eb72cc29aa025bd58b",
+    "url": "/Pratica-react-06-set-state/static/js/main.d6eb72cc.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/Pratica-react-06-set-state/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "6a9ed8bb838c5d13005f",
+    "url": "/Pratica-react-06-set-state/static/js/2.6a9ed8bb.chunk.js"
+  },
+  {
+    "revision": "109049ff36321805a94e",
+    "url": "/Pratica-react-06-set-state/static/js/runtime~main.109049ff.js"
+  },
+  {
+    "revision": "5972eba8338b2dc9cee48067b9c8db1c",
+    "url": "/Pratica-react-06-set-state/static/media/carrinho.5972eba8.webp"
+  },
+  {
+    "revision": "d059cd932a80296078a4069a13c8a401",
+    "url": "/Pratica-react-06-set-state/index.html"
+  }
+];
